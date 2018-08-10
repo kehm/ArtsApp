61c8165fbc7fa0c10cacceee827bbe5cc2658423
Christian Falch <christian.falch@gmail.com>
2018-08-10 11:29:10 +0200

    Updated with support for Flow

--- app/components/KeyHeader/index.js
+++ app/components/KeyHeader/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import styles  from './styles.js';
 
-class KeyHeader extends React.Component {
+type Props = {
+  title: String,
+  closeButtonTitle: String,
+  onClose: String,
+}
+class KeyHeader extends React.Component<Props> {
 
   render() {
+    const { title, closeButtonTitle, onClose } = this.props;
+    
     return (
       <View style={styles.container}>
         <Text>KeyHeader</Text>
