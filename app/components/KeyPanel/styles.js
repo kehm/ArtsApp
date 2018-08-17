5ea67e78c8013f427f2bd80fc12f2331024b794b
Bjørn Egil <bjorn.egil.hansen@gmail.com>
2018-08-17 14:16:41 +0200

    WIP: Frontpage - adjust swiper dots to be visible on Android

--- app/components/KeyPanel/styles.js
+++ app/components/KeyPanel/styles.js
@@ -2,16 +2,14 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    // backgroundColor: '#CC44AA',
   },
   swiper: {
-    // backgroundColor: '#44AACC',
   },
   dotStyle: {
-    transform: [{ translateY: 20}],
+    transform: [{ translateY: 5}],
   },
   activeDot: {
     backgroundColor: '#000',
-    transform: [{ translateY: 20}],
+    transform: [{ translateY: 5}],
   }
 });
