5f6b048163d2f286a1c132c31c7d71c841a2bb15
Christian Falch <christian@framx.no>
2019-03-06 13:09:08 +0100

    Changed text size to be a bit bigger for description texts

--- app/components/Explanation/styles.js
+++ app/components/Explanation/styles.js
@@ -1,13 +1,13 @@
-import { Platform, StyleSheet } from 'react-native';
-import DeviceInfo from 'react-native-device-info';
+import { Platform, StyleSheet } from "react-native";
+import DeviceInfo from "react-native-device-info";
 
 export default StyleSheet.create({
   container: {
     paddingHorizontal: DeviceInfo.isTablet() ? 80 : 20,
-    alignItems: 'center',
+    alignItems: "center"
   },
   description: {
-    fontSize: DeviceInfo.isTablet() ? 16 : 11,
-    textAlign: 'center',
+    fontSize: DeviceInfo.isTablet() ? 16 : 13,
+    textAlign: "center"
   }
 });
