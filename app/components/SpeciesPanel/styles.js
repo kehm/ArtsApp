7cfc8127b5ee981ddb18fd79c07cae1192c98b7f
Christian Falch <christian@framx.no>
2019-03-06 14:17:52 +0100

    Updated with support for iPhoneX rendering.

--- app/components/SpeciesPanel/styles.js
+++ app/components/SpeciesPanel/styles.js
@@ -1,49 +1,50 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import DeviceInfo from "react-native-device-info";
 
 export default StyleSheet.create({
   container: {
-    alignItems: 'stretch',
-    borderTopColor: '#CCC',
-    borderTopWidth: 1,
+    alignItems: "stretch",
+    borderTopColor: "#CCC",
+    borderTopWidth: 1
   },
   panelHeader: {
     paddingLeft: 10,
     paddingTop: 0,
     paddingRight: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    paddingBottom: +(DeviceInfo.hasNotch() ? 25 : 0),
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   emptyDescription: {
     padding: 10,
     height: 44,
-    fontSize: 12,
+    fontSize: 12
   },
   headerTitle: {
     padding: 10,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start"
   },
   headerButton: {
     padding: 10,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end"
   },
   progress: {
-    paddingBottom: 10,
+    marginBottom: 10 + (DeviceInfo.hasNotch() ? 25 : 0),
     paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: 'white',
+    paddingRight: 10
   },
   viewAllContainer: {
     marginRight: 10,
     marginLeft: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 120,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 120
   },
   viewAllIcon: {
-    paddingTop: 12,
+    paddingTop: 12
   },
   viewAllText: {
-    paddingTop: 14,
+    paddingTop: 14
   }
 });
