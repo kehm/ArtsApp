c90a3647314d45e6345727ed9f3d7e5d401d8f54
Christian Falch <christian@framx.no>
2019-03-11 10:38:11 +0100

    Updated backgroundcolor on Key in TraitList

--- app/components/TraitList/styles.js
+++ app/components/TraitList/styles.js
@@ -1,17 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#FFFFFF"
   },
   item: {
-    flex: 1,
+    flex: 1
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   footer: {
-    height: 16,
+    height: 16
   }
 });
